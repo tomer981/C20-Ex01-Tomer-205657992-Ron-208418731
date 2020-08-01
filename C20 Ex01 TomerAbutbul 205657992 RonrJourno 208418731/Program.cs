4cf7c9204ed52209640072e51f9640a4df31ac4e
tomer981 <tomer981@gmail.com>
2020-08-01 18:28:15 +0300

    key

--- C20 Ex01 TomerAbutbul 205657992 RonrJourno 208418731/Program.cs
+++ C20 Ex01 TomerAbutbul 205657992 RonrJourno 208418731/Program.cs
@@ -15,9 +15,9 @@ namespace C20_Ex01_TomerAbutbul_205657992_RonJourno_208418731
         [STAThread]
         static void Main()
         {
+            LoginResult result = FacebookWrapper.FacebookService.Connect("272862089537667");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            LoginResult result = FacebookWrapper.FacebookService.Connect("654371695268712");
             Application.Run(new Form1());
         }
     }
