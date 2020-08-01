ebe5cf0d20d04a12c473c5f919462810d3072af6
tomer981 <tomer981@gmail.com>
2020-08-01 17:45:11 +0300

    start

---
+++ C20 Ex01 TomerAbutbul 205657992 RonrJourno 208418731/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C20_Ex01_TomerAbutbul_205657992_RonrJourno_208418731
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C20 Ex01 TomerAbutbul 205657992 RonrJourno 208418731/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace C20_Ex01_TomerAbutbul_205657992_RonrJourno_208418731
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
