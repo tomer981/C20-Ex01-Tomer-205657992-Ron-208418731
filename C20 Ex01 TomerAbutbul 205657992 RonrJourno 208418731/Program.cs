1e9b9f48b584ffc93f47c886b7e68075c2adfba4
tomer981 <tomer981@gmail.com>
2020-08-08 18:41:16 +0300

    ground zero

--- C20 Ex01 TomerAbutbul 205657992 RonrJourno 208418731/Program.cs
+++ C20 Ex01 TomerAbutbul 205657992 RonrJourno 208418731/Program.cs
@@ -15,8 +15,20 @@ namespace C20_Ex01_TomerAbutbul_205657992_RonJourno_208418731
         [STAThread]
         static void Main()
         {
-            LoginResult result = FacebookWrapper.FacebookService.Connect("654371695268712", "education_schools", "user_Educations");
-            LoginResult result1 = FacebookWrapper.FacebookService.Login("654371695268712", "education_schools", "user_Educations");
+            //LoginResult result = FacebookWrapper.FacebookService.Connect("654371695268712");
+            LoginResult result = FacebookWrapper.FacebookService.Login(
+                "654371695268712",
+                "email",
+                "user_friends",
+                "user_age_range",
+                "user_birthday",
+                "user_gender",
+                "user_hometown",
+                "user_likes",
+                "user_link",
+                "user_photos",
+                "user_posts",
+                "user_videos");
             Console.WriteLine(result);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
